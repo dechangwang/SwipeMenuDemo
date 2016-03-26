6e70cf71fa03b97f11fc92328d1f72193024e7ca
wdchang <13changice@tongji.edu.cn>
2016-03-26 16:13:04 +0800

    Initial commit

---
+++ app/src/main/java/wang/edu/cn/swipemenudemo/MainActivity.java
@@ -0,0 +1,50 @@
+package wang.edu.cn.swipemenudemo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    public void onClick(View v){
+        switch (v.getId()) {
+            case R.id.button1:
+                startActivity(new Intent(this, SimpleActivity.class));
+                break;
+            case R.id.button2:
+                startActivity(new Intent(this, DifferentMenuActivity.class));
+                break;
+        }
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+
+}
---
+++ app/src/main/java/wang/edu/cn/swipemenudemo/swipemenu/SwipeMenuCreator.java
@@ -0,0 +1,13 @@
+package wang.edu.cn.swipemenudemo.swipemenu;
+
+
+/**
+ * 
+ * @author baoyz
+ * @date 2014-8-24
+ *
+ */
+public interface SwipeMenuCreator {
+
+	void create(SwipeMenu menu);
+}
